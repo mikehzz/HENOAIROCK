4fef5e8b86bd1f40ac52ebc2f74ee98f3d369e20
JINKWINE <kwan1218@gmail.com>
2023-07-28 16:33:48 +0900

    게시판 DTO 파일 생성 및 작성_PostDTO.java

---
+++ HENOAIROCK/src/main/java/com/heno/airock/dto/PostDTO.java
@@ -0,0 +1,83 @@
+package com.heno.airock.dto;
+
+public class PostDTO {
+	private String post_seq;
+	private String user_id;
+	private String post_title;
+	private String post_contents;
+	private int read_cnt;
+	private int like_cnt;
+	private String update_dt;
+	private String post_dt;
+	
+	public String getPost_seq() {
+		return post_seq;
+	}
+	public void setPost_seq(String post_seq) {
+		this.post_seq = post_seq;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getPost_title() {
+		return post_title;
+	}
+	public void setPost_title(String post_title) {
+		this.post_title = post_title;
+	}
+	public String getPost_contents() {
+		return post_contents;
+	}
+	public void setPost_contents(String post_contents) {
+		this.post_contents = post_contents;
+	}
+	public int getRead_cnt() {
+		return read_cnt;
+	}
+	public void setRead_cnt(int read_cnt) {
+		this.read_cnt = read_cnt;
+	}
+	public int getLike_cnt() {
+		return like_cnt;
+	}
+	public void setLike_cnt(int like_cnt) {
+		this.like_cnt = like_cnt;
+	}
+	public String getUpdate_dt() {
+		return update_dt;
+	}
+	public void setUpdate_dt(String update_dt) {
+		this.update_dt = update_dt;
+	}
+	public String getPost_dt() {
+		return post_dt;
+	}
+	public void setPost_dt(String post_dt) {
+		this.post_dt = post_dt;
+	}
+	public PostDTO(String post_seq, String user_id, String post_title, String post_contents, int read_cnt, int like_cnt,
+			String update_dt, String post_dt) {
+		super();
+		this.post_seq = post_seq;
+		this.user_id = user_id;
+		this.post_title = post_title;
+		this.post_contents = post_contents;
+		this.read_cnt = read_cnt;
+		this.like_cnt = like_cnt;
+		this.update_dt = update_dt;
+		this.post_dt = post_dt;
+	}
+	@Override
+	public String toString() {
+		return "PostDTO [post_seq=" + post_seq + ", user_id=" + user_id + ", post_title=" + post_title
+				+ ", post_contents=" + post_contents + ", read_cnt=" + read_cnt + ", like_cnt=" + like_cnt
+				+ ", update_dt=" + update_dt + ", post_dt=" + post_dt + "]";
+	}
+	
+	
+}
+
+
