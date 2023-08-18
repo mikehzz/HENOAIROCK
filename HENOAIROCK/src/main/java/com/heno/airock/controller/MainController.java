0b8aec6bfd8a030282f1151f045d8c5979f3e426
PCWK <kjmin1124@naver.com>
2023-08-18 16:16:31 +0900

    main jsp 변경

--- HENOAIROCK/src/main/java/com/heno/airock/controller/MainController.java
+++ HENOAIROCK/src/main/java/com/heno/airock/controller/MainController.java
@@ -3,11 +3,13 @@ package com.heno.airock.controller;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,7 +31,7 @@ public class MainController implements PcwkLoger{
 	
 	@RequestMapping(value = "")
 	public String main(ChatMessageVO inVO, Model model, HttpSession session) throws SQLException {
-		String viewPage = "common/main";
+		String viewPage = "/common/main";
 		MemberDTO memberDTO = (MemberDTO) session.getAttribute("user");
 		
 		if (memberDTO != null) {
@@ -43,12 +45,6 @@ public class MainController implements PcwkLoger{
 			return "redirect:/member/login";
 		}
 		
-	}
-	
-	@RequestMapping(value="/chat",method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-	@ResponseBody
-	public String chat(ChatMessageDetailVO inVO) {
-		return null;
 	}
 	
 	@RequestMapping(value= "/start", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
